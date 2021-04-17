fad228259276dac6c1f71794e1c02a542448843c
Saymon Felipe <linnubr@gmail.com>
2021-04-17 13:03:06 -0300

    Atualização 17/04

---
+++ script.js
@@ -0,0 +1,30 @@
+var container = document.getElementById("slider")
+
+function changeImage() {
+    if (container.classList == "container") {
+        container.classList.remove("container")
+        container.classList.add("container1")
+    }
+    else if (container.classList == "container1") {
+        container.classList.remove("container1")
+        container.classList.add("container2")
+    }
+    else if (container.classList == "container2") {
+        container.classList.remove("container2")
+        container.classList.add("container3")
+    }
+    else if (container.classList == "container3") {
+        container.classList.remove("container3")
+        container.classList.add("container4")
+    }
+    else if (container.classList == "container4") {
+        container.classList.remove("container4")
+        container.classList.add("container5")
+    }
+    else if (container.classList == "container5") {
+        container.classList.remove("container5")
+        container.classList.add("container")
+    }
+
+}
+setInterval(changeImage, 7000)
